48220c5994b6b91d3da056d9bbb2205fa1d51113
Wouter Ensink <wouterensink3@gmail.com>
2020-10-03 14:52:58 +0200

    removed unused functions

--- CSD2a/multi_sample_sequencer/src/main.py
+++ CSD2a/multi_sample_sequencer/src/main.py
@@ -2,30 +2,7 @@
 
 from sequencer import Sequencer
 from console_interface import ConsoleInterface
-from os.path import isfile
-import sys
 import json
-
-
-def exit_with_message(message: str):
-    print(message)
-    exit(-1)
-
-
-# gets the first commandline argument, checks if the file exists and if it's a wav file
-def get_filename() -> str:
-    if len(sys.argv) <= 1:
-        exit_with_message('Please give file (.wav) as command line argument')
-
-    filename = sys.argv[1].strip()
-
-    if not isfile(filename):
-        exit_with_message(f'{filename} is not a file')
-
-    if not filename.endswith('.wav'):
-        exit_with_message(f'Argument doesn\'t have a valid file extension, expected \'.wav\'')
-
-    return filename
 
 
 # resembles the whole sequencer put together, so both ui and the audio part
@@ -39,10 +16,12 @@ class MultiSampleSequencer:
 
 
 def main() -> None:
+    # load the settings json file to use as a project in the sequencer
     with open('../config/settings.json', 'r') as file:
         settings = json.load(file)
         MultiSampleSequencer(settings).run()
 
+    # save the changes made to the settings file (things like adding/removing samples and events)
     with open('../config/settings.json', 'w') as file:
         file.write(json.dumps(settings, indent=4))
 
