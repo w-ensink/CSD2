04b5b79d53449eed28b5ea5eee3d57c0f6cae7e8
Wouter Ensink <wouterensink3@gmail.com>
2020-12-12 14:37:57 +0100

    progess in sequencer

--- CSD2b/console_synth/tests/sequencer_test.cpp
+++ CSD2b/console_synth/tests/sequencer_test.cpp
@@ -34,7 +34,7 @@ struct PlayHead
     void updateStreamTimeRange (juce::Range<double> range)
     {
         if (sessionTimeRangeCurrentBlockMs.getLength() == 0)
-            sessionTimeRangeCurrentBlockMs.setEnd(sessionTimeRangeCurrentBlockMs.getStart() + range.getLength());
+            sessionTimeRangeCurrentBlockMs.setEnd (sessionTimeRangeCurrentBlockMs.getStart() + range.getLength());
         else
             sessionTimeRangeCurrentBlockMs += range.getLength();
         streamTimeRangeCurrentBlockMs = range;
@@ -106,7 +106,6 @@ struct PlaybackContext
 };
 
 
-
 // this tests the translation of streaming time to session time
 // the PlaybackContext calculates the streaming time based on the block size and sample rate
 // and tells the play head about that new streaming time. The play head then translates that to
@@ -146,8 +145,165 @@ TEST_CASE ("single event in second buffer")
     CHECK_THAT (playHead.sessionTimeRangeCurrentBlockMs.getEnd(), Catch::Matchers::WithinAbs (1100, 0.1));
 
     REQUIRE (! context.midiBuffer.isEmpty());
-    auto m = context.midiBuffer.findNextSamplePosition(0);
+    auto m = context.midiBuffer.findNextSamplePosition (0);
 
     // the position relative to the audio buffer should be the 160th sample.
-    REQUIRE((*m).samplePosition == 160);
+    REQUIRE ((*m).samplePosition == 160);
+}
+
+
+namespace tst2
+{
+struct PlayHead
+{
+    // when processing a buffer, measure the
+    double getSessionTime (double timeIntoCurrentRenderPass)
+    {
+    }
+
+    auto getStreamTimeRangeForCurrentRenderPass() const
+    {
+        return streamTimeRangeCurrentBlockMs;
+    }
+
+    auto moveToSessionTime (double sessionTime)
+    {
+        auto positionEditLock = juce::ScopedLock { sessionPositionMutex };
+        currentSessionTime = sessionTime;
+    }
+
+    bool isPlaying() const { return playing; }
+
+private:
+    bool playing;
+    std::optional<juce::Range<double>> loopingRange = std::nullopt;
+    juce::Range<double> streamTimeRangeCurrentBlockMs { 0, 0 };
+    juce::CriticalSection sessionPositionMutex;
+    double currentSessionTime = 0;
+};
+
+
+struct TransportControl
+{
+    void startPlayback() { playState = PlayState::playing; }
+    void stopPlayback() { playState = PlayState::stopped; }
+    void setLoopingRangeMs (double start, double end) {}
+
+    [[nodiscard]] bool isPlaying() const { return playState == PlayState::playing; }
+
+private:
+    enum struct PlayState
+    {
+        playing,
+        stopped,
+        recording
+    };
+
+    PlayState playState = PlayState::stopped;
+
+    double position = 0;
+};
+
+
+// this should provide playback context with midi from any source (internal and external via midi collector)
+// the midi source that plays back the melody should consider the play head,
+// the midi source that provides external midi should not care about the play head
+struct MidiSource
+{
+    void fillNextMidiBuffer (const PlayHead& playHead, juce::MidiBuffer& bufferToFill, double numSamples)
+    {
+    }
+
+    std::vector<tst::Event> events;
+};
+
+
+struct AudioSource
+{
+    void fillNextBuffer (const PlayHead& playHead, juce::AudioBuffer<float>& audioBuffer, juce::MidiBuffer& midiBuffer)
+    {
+    }
+};
+
+
+// this should be called from the audio device callback with the current stream time (ms) and the audio buffer to fill
+struct PlaybackContext
+{
+    PlaybackContext (TransportControl& tc, AudioSource& as) : audioSource { as }, transportControl { tc } {}
+
+    void prepare (double rate)
+    {
+        sampleRate = rate;
+    }
+
+    void fillNextAudioBlock (juce::Range<double> deviceStreamTimeRange, juce::AudioBuffer<float>& bufferToFill)
+    {
+        midiBuffer.clear();
+
+        std::for_each (midiSources.begin(), midiSources.end(), [&] (MidiSource* midiSource) {
+            midiSource->fillNextMidiBuffer (playHead, midiBuffer, bufferToFill.getNumSamples());
+        });
+
+        audioSource.fillNextBuffer (playHead, bufferToFill, midiBuffer);
+    }
+
+    double sampleRate = 0;
+    juce::MidiBuffer midiBuffer;
+    std::vector<MidiSource*> midiSources;
+    AudioSource& audioSource;
+    PlayHead playHead;
+    TransportControl& transportControl;
+};
+
+
+struct AudioDeviceMock
+{
+    double sampleRate;
+    int blockSize;
+    PlaybackContext& context;
+    juce::Range<double> currentStreamTimeRange { 0, 0 };
+
+    void init()
+    {
+        currentStreamTimeRange = { 2000.0, 2000.0 + (blockSize / sampleRate) * 1000 };
+        context.prepare (sampleRate);
+    }
+
+    void simulateDeviceCallback()
+    {
+        auto block = makeZeroedBufferOfSize (blockSize);
+        context.fillNextAudioBlock (currentStreamTimeRange, block);
+        currentStreamTimeRange += (blockSize / sampleRate) * 1000;
+    }
+};
+
+
+}  // namespace tst2
+
+
+TEST_CASE ("new play head test")
+{
+    auto blockSize = 200;                                // samples
+    auto events = std::vector<tst::Event> { { 1000 } };  // one event at time stamp 1000
+    auto sampleRate = 400.0;                             // samples per second
+
+    auto transportControl = tst2::TransportControl {};
+    auto audioSource = tst2::AudioSource {};
+    auto context = tst2::PlaybackContext (transportControl, audioSource);
+
+    auto deviceMock = tst2::AudioDeviceMock {
+        .sampleRate = sampleRate,
+        .blockSize = blockSize,
+        .context = context
+    };
+
+    auto midiSource = tst2::MidiSource();
+    midiSource.events.push_back (tst::Event { 1000 });
+
+    context.midiSources.push_back (&midiSource);
+
+    REQUIRE (context.midiBuffer.isEmpty());
+
+
+    REQUIRE (context.midiBuffer.isEmpty());
 }
